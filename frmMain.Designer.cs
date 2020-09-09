2817135e7074907daa1cd9c587eb3ef6a69aee8d
Douglas Spencer <douglasdevelops@gmail.com>
2020-09-09 18:10:03 -0400

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace KeypadSolver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUpstageCenter = new System.Windows.Forms.Label();
+            this.lblDownStageRight = new System.Windows.Forms.Label();
+            this.lblStageRight = new System.Windows.Forms.Label();
+            this.lblStageCenter = new System.Windows.Forms.Label();
+            this.lblDownstageCenter = new System.Windows.Forms.Label();
+            this.lblUpstageLeft = new System.Windows.Forms.Label();
+            this.lblStageLeft = new System.Windows.Forms.Label();
+            this.lblDownstageLeft = new System.Windows.Forms.Label();
+            this.lblUpstageRight = new System.Windows.Forms.Label();
+            this.btnMoveLetters = new System.Windows.Forms.Button();
+            this.txtInstructions = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblUpstageCenter);
+            this.panel1.Controls.Add(this.lblDownStageRight);
+            this.panel1.Controls.Add(this.lblStageRight);
+            this.panel1.Controls.Add(this.lblStageCenter);
+            this.panel1.Controls.Add(this.lblDownstageCenter);
+            this.panel1.Controls.Add(this.lblUpstageLeft);
+            this.panel1.Controls.Add(this.lblStageLeft);
+            this.panel1.Controls.Add(this.lblDownstageLeft);
+            this.panel1.Controls.Add(this.lblUpstageRight);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 233);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblUpstageCenter
+            // 
+            this.lblUpstageCenter.AutoSize = true;
+            this.lblUpstageCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpstageCenter.Location = new System.Drawing.Point(167, 37);
+            this.lblUpstageCenter.Name = "lblUpstageCenter";
+            this.lblUpstageCenter.Size = new System.Drawing.Size(95, 13);
+            this.lblUpstageCenter.TabIndex = 8;
+            this.lblUpstageCenter.Tag = "DEF";
+            this.lblUpstageCenter.Text = "Upstage Center";
+            // 
+            // lblDownStageRight
+            // 
+            this.lblDownStageRight.AutoSize = true;
+            this.lblDownStageRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownStageRight.Location = new System.Drawing.Point(35, 193);
+            this.lblDownStageRight.Name = "lblDownStageRight";
+            this.lblDownStageRight.Size = new System.Drawing.Size(104, 13);
+            this.lblDownStageRight.TabIndex = 7;
+            this.lblDownStageRight.Tag = "STU";
+            this.lblDownStageRight.Text = "Downstage Right";
+            // 
+            // lblStageRight
+            // 
+            this.lblStageRight.AutoSize = true;
+            this.lblStageRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStageRight.Location = new System.Drawing.Point(35, 122);
+            this.lblStageRight.Name = "lblStageRight";
+            this.lblStageRight.Size = new System.Drawing.Size(74, 13);
+            this.lblStageRight.TabIndex = 6;
+            this.lblStageRight.Tag = "JKL";
+            this.lblStageRight.Text = "Stage Right";
+            // 
+            // lblStageCenter
+            // 
+            this.lblStageCenter.AutoSize = true;
+            this.lblStageCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStageCenter.Location = new System.Drawing.Point(167, 122);
+            this.lblStageCenter.Name = "lblStageCenter";
+            this.lblStageCenter.Size = new System.Drawing.Size(81, 13);
+            this.lblStageCenter.TabIndex = 5;
+            this.lblStageCenter.Tag = "MNO";
+            this.lblStageCenter.Text = "Stage Center";
+            // 
+            // lblDownstageCenter
+            // 
+            this.lblDownstageCenter.AutoSize = true;
+            this.lblDownstageCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownstageCenter.Location = new System.Drawing.Point(167, 193);
+            this.lblDownstageCenter.Name = "lblDownstageCenter";
+            this.lblDownstageCenter.Size = new System.Drawing.Size(111, 13);
+            this.lblDownstageCenter.TabIndex = 4;
+            this.lblDownstageCenter.Tag = "VWX";
+            this.lblDownstageCenter.Text = "Downstage Center";
+            // 
+            // lblUpstageLeft
+            // 
+            this.lblUpstageLeft.AutoSize = true;
+            this.lblUpstageLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpstageLeft.Location = new System.Drawing.Point(300, 37);
+            this.lblUpstageLeft.Name = "lblUpstageLeft";
+            this.lblUpstageLeft.Size = new System.Drawing.Size(80, 13);
+            this.lblUpstageLeft.TabIndex = 3;
+            this.lblUpstageLeft.Tag = "GHI";
+            this.lblUpstageLeft.Text = "Upstage Left";
+            // 
+            // lblStageLeft
+            // 
+            this.lblStageLeft.AutoSize = true;
+            this.lblStageLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStageLeft.Location = new System.Drawing.Point(300, 122);
+            this.lblStageLeft.Name = "lblStageLeft";
+            this.lblStageLeft.Size = new System.Drawing.Size(66, 13);
+            this.lblStageLeft.TabIndex = 2;
+            this.lblStageLeft.Tag = "PQR";
+            this.lblStageLeft.Text = "Stage Left";
+            // 
+            // lblDownstageLeft
+            // 
+            this.lblDownstageLeft.AutoSize = true;
+            this.lblDownstageLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownstageLeft.Location = new System.Drawing.Point(290, 193);
+            this.lblDownstageLeft.Name = "lblDownstageLeft";
+            this.lblDownstageLeft.Size = new System.Drawing.Size(96, 13);
+            this.lblDownstageLeft.TabIndex = 1;
+            this.lblDownstageLeft.Tag = "YZ";
+            this.lblDownstageLeft.Text = "Downstage Left";
+            // 
+            // lblUpstageRight
+            // 
+            this.lblUpstageRight.AutoSize = true;
+            this.lblUpstageRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpstageRight.Location = new System.Drawing.Point(35, 37);
+            this.lblUpstageRight.Name = "lblUpstageRight";
+            this.lblUpstageRight.Size = new System.Drawing.Size(88, 13);
+            this.lblUpstageRight.TabIndex = 0;
+            this.lblUpstageRight.Tag = "ABC";
+            this.lblUpstageRight.Text = "Upstage Right";
+            // 
+            // btnMoveLetters
+            // 
+            this.btnMoveLetters.Location = new System.Drawing.Point(12, 274);
+            this.btnMoveLetters.Name = "btnMoveLetters";
+            this.btnMoveLetters.Size = new System.Drawing.Size(93, 23);
+            this.btnMoveLetters.TabIndex = 1;
+            this.btnMoveLetters.Text = "Move Letters";
+            this.btnMoveLetters.UseVisualStyleBackColor = true;
+            this.btnMoveLetters.Click += new System.EventHandler(this.BtnMoveLetters_Click);
+            // 
+            // txtInstructions
+            // 
+            this.txtInstructions.Location = new System.Drawing.Point(414, 25);
+            this.txtInstructions.Multiline = true;
+            this.txtInstructions.Name = "txtInstructions";
+            this.txtInstructions.Size = new System.Drawing.Size(374, 233);
+            this.txtInstructions.TabIndex = 2;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(414, 274);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(96, 23);
+            this.btnDecode.TabIndex = 3;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.BtnDecode_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(411, 300);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "label1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtInstructions);
+            this.Controls.Add(this.btnMoveLetters);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Text = "Hunt A Killer Keypad Decoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUpstageCenter;
+        private System.Windows.Forms.Label lblDownStageRight;
+        private System.Windows.Forms.Label lblStageRight;
+        private System.Windows.Forms.Label lblStageCenter;
+        private System.Windows.Forms.Label lblDownstageCenter;
+        private System.Windows.Forms.Label lblUpstageLeft;
+        private System.Windows.Forms.Label lblStageLeft;
+        private System.Windows.Forms.Label lblDownstageLeft;
+        private System.Windows.Forms.Label lblUpstageRight;
+        private System.Windows.Forms.Button btnMoveLetters;
+        private System.Windows.Forms.TextBox txtInstructions;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeypadSolver
+{
+   
+    public partial class frmMain : Form
+    {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZZ";
+        List<KeyPadBlock> keypadBlocks = new List<KeyPadBlock>();
+
+        //Stage Direction Indices
+        const int UR = 0;
+        const int UC = 1;
+        const int UL = 2;
+        const int SR = 3;
+        const int SC = 4;
+        const int SL = 5;
+        const int DR = 6;
+        const int DC = 7;
+        const int DL = 8;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            BuildLetterGroups();
+            BuildStageArray();
+        }
+
+        /// <summary>
+        /// Build 
+        /// </summary>
+        private void BuildLetterGroups()
+        {
+            int cntr = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                keypadBlocks.Add(new KeyPadBlock
+                {
+                    LetterGroup = alphabet.Substring(cntr, 3)
+                    
+                });
+
+                cntr += 3;
+            }
+        }
+
+        private void BuildStageArray()
+        {
+            keypadBlocks[0].KeyPadLockLabel = lblUpstageRight;
+            keypadBlocks[0].DisplayText = lblUpstageRight.Text;
+
+            keypadBlocks[1].KeyPadLockLabel = lblUpstageCenter;
+            keypadBlocks[1].DisplayText = lblUpstageCenter.Text;
+
+            keypadBlocks[2].KeyPadLockLabel = lblUpstageLeft;
+            keypadBlocks[2].DisplayText = lblUpstageLeft.Text;
+
+            keypadBlocks[3].KeyPadLockLabel = lblStageRight;
+            keypadBlocks[3].DisplayText = lblStageRight.Text;
+
+            keypadBlocks[4].KeyPadLockLabel = lblStageCenter;
+            keypadBlocks[4].DisplayText = lblStageCenter.Text;
+
+            keypadBlocks[5].KeyPadLockLabel = lblStageLeft;
+            keypadBlocks[5].DisplayText = lblStageLeft.Text;
+
+            keypadBlocks[6].KeyPadLockLabel = lblDownStageRight;
+            keypadBlocks[6].DisplayText = lblDownStageRight.Text;
+
+            keypadBlocks[7].KeyPadLockLabel = lblDownstageCenter;
+            keypadBlocks[7].DisplayText = lblDownstageCenter.Text;
+
+            keypadBlocks[8].KeyPadLockLabel = lblDownstageLeft;
+            keypadBlocks[8].DisplayText = lblDownstageLeft.Text;
+
+            for (int i = 0; i < keypadBlocks.Count; i++)
+            {
+                keypadBlocks[i].KeyPadLockLabel.Tag = keypadBlocks[i].LetterGroup;
+                keypadBlocks[i].KeyPadLockLabel.Text = keypadBlocks[i].LetterGroup;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblOutput.Text = String.Empty;
+        }
+
+        private void BtnMoveLetters_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+
+            temp = keypadBlocks[0].LetterGroup;
+
+            for (int i = 0; i < keypadBlocks.Count - 1; i++)
+            {
+                keypadBlocks[i].LetterGroup = keypadBlocks[i + 1].LetterGroup;
+                keypadBlocks[i].Tag = String.Empty;
+                keypadBlocks[i].DisplayText = Regex.Replace(keypadBlocks[i].DisplayText, @"\(.*?\)", "");
+                keypadBlocks[i].DisplayText = Regex.Replace(keypadBlocks[i].DisplayText, @"\s+", " ");
+                keypadBlocks[i].DisplayText = Regex.Replace(keypadBlocks[i].DisplayText, @"\s+", " ");
+            }
+
+            keypadBlocks[keypadBlocks.Count - 1].LetterGroup = temp;
+
+            BuildStageArray();
+        }
+
+        /// <summary>
+        /// Given a stage direction and a letter in the trio of letters, return decoded character
+        /// </summary>
+        /// <param name="stageDirection">Stage Direction</param>
+        /// <param name="characterNumber">Character Index Number</param>
+        /// <returns></returns>
+        private char FindCharacter(string stageDirection, int characterNumber)
+        {
+            switch (stageDirection)
+            {
+                case "UR":
+                    return keypadBlocks[UR].LetterGroup[characterNumber];
+                case "UC":
+                    return keypadBlocks[UC].LetterGroup[characterNumber];
+                case "UL":
+                    return keypadBlocks[UL].LetterGroup[characterNumber];
+                case "SR":
+                    return keypadBlocks[SR].LetterGroup[characterNumber];
+                case "SC":
+                    return keypadBlocks[SC].LetterGroup[characterNumber];
+                case "SL":
+                    return keypadBlocks[SL].LetterGroup[characterNumber];
+                case "DR":
+                    return keypadBlocks[DR].LetterGroup[characterNumber];
+                case "DC":
+                    return keypadBlocks[DC].LetterGroup[characterNumber];
+                case "DL":
+                    return keypadBlocks[DL].LetterGroup[characterNumber];
+                default:
+                    
+                    break;
+            }
+
+            return ' ';
+        }
+
+        private void BtnDecode_Click(object sender, EventArgs e)
+        {
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < txtInstructions.Lines.Length; i++)
+            {
+                if (txtInstructions.Lines[i] == "")
+                {
+                    continue;
+                }
+                string[] setsToDecode = txtInstructions.Lines[i].Split(new char[] { ' ' });
+                string decodedString = "";
+
+                for (int x = 0; x < setsToDecode.Length; x++)
+                {
+                    int character_number = Convert.ToInt32(setsToDecode[x][2].ToString());
+                    character_number -= 1;
+
+                    char decodedCharacter = FindCharacter(setsToDecode[x][0].ToString() + setsToDecode[x][1].ToString(), character_number);
+                    decodedString += decodedCharacter;
+                }
+
+                output.Add(decodedString);
+            }
+
+            string finalOutput = String.Join(System.Environment.NewLine, output);
+
+            lblOutput.Text = finalOutput;
+
+
+        }
+    }
+
+    /// <summary>
+    /// Class representing one block on the 3X3 Grid
+    /// </summary>
+    internal class KeyPadBlock
+    {
+        public string LetterGroup;
+        public string Tag;
+        public string DisplayText;
+        public Label KeyPadLockLabel;
+
+    }
+}
